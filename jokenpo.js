62ad755a3ac01a5200602cadfbceb93646f34ba1
Beatriz Moreli <beatrizmoreli.01@gmail.com>
2023-12-20 17:24:57 -0300

    Basic Jokenpo

---
+++ jokenpo.js
@@ -0,0 +1,32 @@
+function jogar() {
+    if (document.getElementById("pedra").checked == false && document.getElementById("papel").checked == false && document.getElementById("tesoura").checked == false) {
+        alert("Selecione uma opção");
+    } else {
+
+        var sorteio = Math.floor(Math.random() * 3);
+        switch (sorteio) {
+            case 0:
+                document.getElementById("pc").src = "/images/pcpedra.png";
+                break;
+            case 1:
+                document.getElementById("pc").src = "/images/pcpapel.png";
+                break;
+            case 2:
+                document.getElementById("pc").src = "/images/pctesoura.png";
+                break;
+        }
+
+        if ((document.getElementById("pedra").checked == true && sorteio == 0) || (document.getElementById("papel").checked == true && sorteio == 1) || (document.getElementById("tesoura").checked == true && sorteio == 2)) {
+            document.getElementById("placar").innerHTML = "Empate";
+        } else if ((document.getElementById("pedra").checked == true && sorteio == 2) || (document.getElementById("papel").checked == true && sorteio == 0) || (document.getElementById("tesoura").checked == true && sorteio == 1)) {
+            document.getElementById("placar").innerHTML = "Jogador Venceu";
+        } else {
+            document.getElementById("placar").innerHTML = "Computador Venceu";
+        }
+    }
+}
+
+function resetar() {
+    document.getElementById("pc").src = "/images/pc.png";
+    document.getElementById("placar").innerHTML = "";
+}
